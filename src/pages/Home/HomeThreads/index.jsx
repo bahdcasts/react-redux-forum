53a951efe2c203d9c27c02122c6baad987e7a8ce
Kati Frantz <frantzenterprises777@gmail.com>
2018-05-04 05:24:41 +0100

    add redux dev tools extension support add GET_CHANNELS handling in reducer

--- src/pages/Home/HomeThreads/index.jsx
+++ src/pages/Home/HomeThreads/index.jsx
@@ -38,12 +38,12 @@ const HomeThreads = () => (
     </div>
     <div className="row justify-content-center my-5">
       <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item"><a class="page-link" href="">Previous</a></li>
-          <li class="page-item"><a class="page-link" href="">1</a></li>
-          <li class="page-item"><a class="page-link" href="">2</a></li>
-          <li class="page-item"><a class="page-link" href="">3</a></li>
-          <li class="page-item"><a class="page-link" href="">Next</a></li>
+        <ul className="pagination">
+          <li className="page-item"><a className="page-link" href="">Previous</a></li>
+          <li className="page-item"><a className="page-link" href="">1</a></li>
+          <li className="page-item"><a className="page-link" href="">2</a></li>
+          <li className="page-item"><a className="page-link" href="">3</a></li>
+          <li className="page-item"><a className="page-link" href="">Next</a></li>
         </ul>
       </nav>
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,32 @@
 import { createStore } from 'redux'
 
-const reducer = (state = {}, action) => {
+const reducer = (state, action) => {
+  if (action.type === 'GET_CHANNELS') {
+    return {
+      channels: [{
+        id: 1,
+        name: 'angular'
+      }, {
+        id: 2,
+        name: 'laravel'
+      }]
+    }
+  }
   return state
 }
 
-const store = createStore(reducer)
+const initialState = {
+  channels: []
+}
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.dispatch({
+  type: 'GET_CHANNELS'
+})
 
 export default store
