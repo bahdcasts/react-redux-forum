093ee1572dab03a7aeb5994c4d2cf3b0bdb54133
Kati Frantz <frantzenterprises777@gmail.com>
2018-05-04 05:12:06 +0100

    install redux install react-redux create store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, withRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter, withRouter } from 'react-router-dom';
+import store from './store';
 
 const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <AppWithRouter />
-  </BrowserRouter>  
-, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppWithRouter />
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux'
+
+const reducer = (state = {}, action) => {
+  return state
+}
+
+const store = createStore(reducer)
+
+export default store
