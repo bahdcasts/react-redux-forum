fd0d1764f8079e7575383cbe83254d193221ac3e
Kati Frantz <frantzenterprises777@gmail.com>
2018-05-05 01:08:51 +0100

    install gravatar install date-fns format dates

---
+++ src/components/Thread/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Gravatar from 'react-gravatar'
+import distanceInWordsStrict from 'date-fns/distance_in_words_strict'
+
+const Thread = ({ thread }) => ((
+  <div className="card mb-3">
+    <div className="card-header">
+      <Gravatar email={thread.creator.email} className="mr-3 rounded-circle" width="30px" height="30px" />
+      <span className="text-sm text-muted">{thread.creator.name}, <b>{distanceInWordsStrict(new Date(), thread.created_at)} ago</b></span>
+      <a href="" className="btn btn-info btn-xs float-right">view thread</a>
+    </div>
+    <div className="card-body">
+      <h5 className="text-center">{thread.title}</h5>
+    </div>
+    <div className="card-footer text-muted">
+      <span>{thread.replies_count} replies</span>
+      <a href="" className="btn btn-primary float-right btn-xs">{thread.channel.name}</a>
+    </div>
+  </div>
+))
+
+export default Thread
--- src/pages/Home/HomeThreads/index.jsx
+++ src/pages/Home/HomeThreads/index.jsx
@@ -1,24 +1,11 @@
 import React, { Fragment } from 'react';
 import ReactPaginate from 'react-paginate';
 
+import Thread from '../../../components/Thread'
+
 const HomeThreads = ({ threads, handlePageChange, pageCount, currentPage }) => (
   <Fragment>
-    {threads.map(thread => (
-      <div key={thread.id} className="card mb-3">
-        <div className="card-header">
-          <img src="http://i.pravatar.cc/300" className="mr-3 rounded-circle" width="30px" height="30px" alt="" />
-          <span className="text-sm text-muted">{thread.creator.name}, <b>3 hrs ago</b></span>
-          <a href="" className="btn btn-info btn-xs float-right">view thread</a>
-        </div>
-        <div className="card-body">
-          <h5 className="text-center">{thread.title}</h5>
-        </div>
-        <div className="card-footer text-muted">
-          <span>{thread.replies_count} replies</span>
-          <a href="" className="btn btn-primary float-right btn-xs">{thread.channel.name}</a>
-        </div>
-      </div>
-    ))}
+    {threads.map(thread => <Thread thread={thread} key={thread.id} />)}
     <div className="row justify-content-center my-5">
       <ReactPaginate
         containerClassName="pagination"
